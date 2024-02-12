027233181b5e93909a1ef9a379258884584fe991
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-02-12 15:18:40 +0000

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12222
+let accountEmail = "abhishek"
+var accountPassword = "12t43"
+accountcity = "jaipur"
+
+//never use without kyeword and not use var because var is proble in block scope and functionl scope
+
+accountEmail = "bfbsf.ckm"
+accountPassword = "kjbfwfk"
+accountcity = "aptna"
+
+console.log(accountId)
+
+
+console.table([accountEmail ,accountId ,accountPassword,accountcity])
