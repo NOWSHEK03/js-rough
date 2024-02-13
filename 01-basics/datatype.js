73e2e13840e1c3f1da4ebaec767e024dc78e309e
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-02-13 07:47:55 +0000

    datatype

---
+++ 01-basics/datatype.js
@@ -0,0 +1,11 @@
+//types of datatypes 
+// number 
+// string ""
+// boolean true / false
+//null standalone value (object)
+// unedfined = undefinde
+// symbole unique
+
+// object
+
+// how to konw type of data type  console.log(typeof x)
