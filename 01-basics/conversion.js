94d1bc9ac14e6333e3c18fc0052933a890705dca
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-02-13 08:36:30 +0000

    conversion

---
+++ 01-basics/conversion.js
@@ -0,0 +1,29 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+
+// change in string 
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber)
+// console.log (valueInNumber)
+
+//cheack the valueInNumber of datatype
+
+// NaN is not a number
+
+  // "33"=> gives number
+  // "33abc" => NaN
+  // true => 1; false = 0
+  // null = 0
+  // undefined = undefined
+
+
+let isLoggedIn = "abhishek"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false
+//  "" = false 
+//  "abhisher" = false
