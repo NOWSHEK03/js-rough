fdacb0e10d2d83b3f72e2bbaba7a56d4c28fbb8f
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-02-18 17:03:23 +0000

    date

---
+++ 01-basics/date.js
@@ -0,0 +1,16 @@
+// date
+
+let myDate = new Date()
+//  console.log(myDate.toString());
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toDateString());
+//  console.log(typeof myDate);
+ 
+// create specific date
+
+//let mycreatedDate = new Date(2033 , 0 , 23 )
+// console.log(mycreatedDate);
+
+//for date yy/mm/dd//
+ let mycreatedDate = new Date("2023-01-14")
+ console.log(mycreatedDate.toDateString());
