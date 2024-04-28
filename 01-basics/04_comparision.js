28a65b779c272991f5e5f1ea8b72de5f3c434d17
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-04-28 13:23:08 +0000

    comparision

---
+++ 01-basics/04_comparision.js
@@ -0,0 +1,29 @@
+
+// basics camparision 
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// // === for not only comparision also cheak datatype
+
+// console.log("2" === 2);
+
+
+
+
+
