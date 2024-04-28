731ef4c8287fb18fc635655e0ba813cf29916f8f
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-04-28 07:11:49 +0000

    file change name

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+
+// change in string 
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber)
+// console.log (valueInNumber)
+
+//cheack the valueInNumber of datatype
+
+// NaN is not a number
+
+  // "33"=> gives number
+  // "33abc" => NaN
+  // true => 1; false = 0
+  // null = 0
+  // undefined = undefined
+
+
+let isLoggedIn = "abhishek"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false
+//  "" = false 
+//  "abhisher" = false
+
+// *****************************operation*****************************
+
+  let value = 3
+   let negValue = -value
+   // console.log(negValue);
+ 
+//    console.log(2+2)
+//    console.log(2-2)
+//    console.log(2*2)
+//    console.log(2**2) power
+//    console.log(2/2)
+//    console.log(2%2) remender
