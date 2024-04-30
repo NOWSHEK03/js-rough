a5398190467685d63bc8275833fdb057e0c97a62
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-04-30 00:05:51 +0000

    string

--- 01-basics/String.js
+++ 01-basics/String.js
@@ -6,10 +6,10 @@
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  note : morden version
 
-const gameName = new String('hitesh-hc-com')
+// const gameName = new String('hitesh-hc-com')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
 // // console.log(gameName.length);
@@ -36,10 +36,15 @@ console.log(gameName.__proto__);
 // console.log(gameName.split('-'));
 
 
-// const family = ' papa '
+///////////////////////////////////////////=======================================//////////////////////////////////////////
 
-// console.log(family)
-// console.log(family.toUpperCase)
+
+const familyName = new String ("kfvkn")
+
+
+console.log(familyName);
+console.log(familyName.toUpperCase())
+
 
 
 
