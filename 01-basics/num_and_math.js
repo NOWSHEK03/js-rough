2c474680d0cc5f8413c81d192895d8a2ed31cb06
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-04-30 14:04:23 +0000

    note update

--- 01-basics/num_and_math.js
+++ 01-basics/num_and_math.js
@@ -26,8 +26,8 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-// console.log(Math.random());
-// console.log((Math.random()*10) + 1);
+// console.log(Math.random()); note : give 0 to 1 
+// console.log((Math.random()*10) + 1); note give two number before 2 degite
 // console.log(Math.floor(Math.random()*10) + 1);
 
 // const min = 10
