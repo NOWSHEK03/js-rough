f5551a729694fe705831144f9d003f9079a9ea2e
NOWSHEK03 <154260809+NOWSHEK03@users.noreply.github.com>
2024-04-30 14:00:59 +0000

    concept clear

--- 01-basics/num_and_math.js
+++ 01-basics/num_and_math.js
@@ -1,8 +1,10 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+// const balance = new Number(100)
 // console.log(balance);
+// console.log(balance.toString);
+// console.log(typeof balance)
 
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1));
@@ -12,9 +14,9 @@ const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN')); 
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+// +++++++++++++ Maths mathod +++++++++++++++++++++++++++++
 
 // console.log(Math);
 // console.log(Math.abs(-4));
@@ -24,11 +26,11 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10
-const max = 20
+// const min = 10
+// const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min) // note : formula
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min) // note : formula
